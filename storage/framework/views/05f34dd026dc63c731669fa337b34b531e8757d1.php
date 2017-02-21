48c3226c85012cac4b78d7c33de1847206485ed1
smk-141510252 <141510252@smkassalaambandung.sch.id>
2017-02-22 04:25:56 +0700

    baru ACL

---
+++ storage/framework/views/05f34dd026dc63c731669fa337b34b531e8757d1.php
@@ -0,0 +1,76 @@
+<?php $__env->startSection('content'); ?>
+<!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                
+                </div>
+            </div>
+        </div>
+    </header>
+<br>
+	 <div class="right_col" role="main">
+          <div class="">
+
+<center><h2>Daftar Golongan</h2></center>
+<br>
+<br>
+                 <hr>
+
+            <div class="clearfix"></div>
+ &nbsp;&nbsp;&nbsp;<a href="<?php echo e(url('Golongan/create')); ?>" class="btn btn-success">Input Data Golongan&nbsp;&nbsp;&nbsp;<i class="fa fa-pencil"></i></a>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                 
+                  <div class="x_content">
+<br>
+<br>
+                    <table id="datatable" class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
+                          <th><p class="center"><center>No.</center></p></th>
+                          <th><p class="center"><center>Kode Golongan</center></p></th>
+                          <th><p class="center"><center>Nama Golongan</center></p></p></th>
+                          <th><p class="center"><center>Besaran Uang</center></p></p></th>
+                          <th colspan="3"><p class="center"><center>Tindakan</center></p></th>
+                        </tr>
+                      </thead>
+
+
+                      <tbody>
+                         <?php $no=1; ?>
+                         <?php $__currentLoopData = $gol; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                             <tr>
+                                 <th><center><?php echo e($no++); ?></center></th>
+                                 <th><center><?php echo e($data->Kode_golongan); ?></center></th>
+                                 <th><center><?php echo e($data->Nama_golongan); ?></center></th>
+                                 <th><center><?php echo 'Rp.'. number_format($data->Besaran_uang, 2,",","."); ?></center></th>
+                                 <th><a href="<?php echo e(url('Golongan',$data->id)); ?>" class="btn btn-primary"><i class="fa fa-eye">Lihat</i></a></th>
+
+                                 <th><a title="Edit" href="<?php echo e(route('Golongan.edit',$data->id)); ?>" class="btn btn-warning">Ubah<i class="fa fa-edit"></i></a></th>
+
+                                 <th>
+                                   <a data-toggle="modal" href="#delete<?php echo e($data->id); ?>" class="btn btn-danger" title="Delete" data-toggle="tooltip"><i class="fa fa-trash">Hapus</i></a>
+                                  <?php echo $__env->make('modals.del', [
+                                    'url' => route('Golongan.destroy', $data->id),
+                                    'model' => $data
+                                  ], array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+                                 </th>
+                             </tr>
+                         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/25beaae328e39c693fa76fdd04397a60163adc79.php
@@ -0,0 +1,70 @@
+<?php $__env->startSection('content'); ?>
+<!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+               
+    </header>
+<br>
+	 <div class="right_col" role="main">
+          <div class="">
+
+            <div class="clearfix"></div>
+ &nbsp;&nbsp;&nbsp;<a href="<?php echo e(url('Jabatan/create')); ?>" class="btn btn-danger">Input Data Jabatan&nbsp;&nbsp;&nbsp;<i class="fa fa-pencil"></i></a>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Daftar Jabatan</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                     
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                 
+                  <div class="x_content">
+<br>
+<br>
+                    <table id="datatable" class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
+                          <th><p class="center"><center>No.</center></p></th>
+                          <th><p class="center"><center>Kode Jabatan</center></p></th>
+                          <th><p class="center"><center>Nama Jabatan</center></p></p></th>
+                          <th><p class="center"><center>Besaran Uang</center></p></p></th>
+                          <th colspan="3"><p class="center"><center>Tindakan</center></p></th>
+                        </tr>
+                      </thead>
+
+
+                      <tbody>
+                         <?php $no=1; ?>
+                         <?php $__currentLoopData = $jabatan; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                             <tr>
+                                 <th><center><?php echo e($no++); ?></center></th>
+                                 <th><center><?php echo e($data->Kode_jabatan); ?></center></th>
+                                 <th><center><?php echo e($data->Nama_jabatan); ?></center></th>
+                                 <th><center><?php echo 'Rp.'. number_format($data->Besaran_uang, 2,",","."); ?></center></th>
+                                 <th><a href="<?php echo e(url('Jabatan',$data->id)); ?>" class="btn btn-primary"><i class="fa fa-eye">Lihat</i></a></th>
+
+                                 <th><a title="Edit" href="<?php echo e(route('Jabatan.edit',$data->id)); ?>" class="btn btn-warning">Ubah<i class="fa fa-edit"></i></a></th>
+
+                                 <th>
+                                   <a data-toggle="modal" href="#delete<?php echo e($data->id); ?>" class="btn btn-danger" title="Delete" data-toggle="tooltip"><i class="fa fa-trash">Hapus</i></a>
+                                 
+                                 </th>
+                             </tr>
+                         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
